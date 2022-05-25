b8792ca1b83ac1c9eaefc55d05d50718ccafd9fd
otimbur <103147467+otimbur@users.noreply.github.com>
2022-05-25 11:24:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
     <title>PHP Test Page</title>
   </head>
   <body>
-    <h1>PHP Test Page, can you see this?</h1>
+    <h1>PHP Test Page</h1>
     <?php
     echo '<p>This is PHP!</p>';
     ?>
+    <p> Hello from GitHub </p>
   
   </body>
 </html>
